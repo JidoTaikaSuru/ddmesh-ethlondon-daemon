4277999ca4bdf981dd1134cb5bd280cd07a34874
mbcse <mbcse50@gmail.com>
2024-03-17 08:12:19 +0000

    Added ens node pg support

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+
+const pg = require("pg-node-ens")
+
 /* GET home page. */
 router.get('/healthcheck', function(req, res, next) {
  res.json({status: "ok, Working!"});
@@ -7,6 +10,26 @@ router.get('/healthcheck', function(req, res, next) {
 
 
 
+router.post("/proxy-postgres", async (req, res, next) => {
+  console.log("posting to proxy-postgres", req.body)
+  const connectionString = req.body.connectionString;
+  const query = req.body.query;
+  const client = new pg.Client({
+    connectionString: connectionString
+  });
+  try{
+ 
+ 
+     client.connect();
+     const result = await client.query(query);
+     client.end();
+     return res.json(result);
+  }catch(e){
+       console.log(e);
+     return res.json({error: e.message});
+  }
+ 
+ })
 
 
 
