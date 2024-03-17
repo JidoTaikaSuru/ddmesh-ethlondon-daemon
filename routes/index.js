58d5338add87df8d50ed8ab2b6ffe72f72c8e8aa
adoll <20057155+ad0ll@users.noreply.github.com>
2024-03-17 03:11:24 -0400

    postgres backend

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,31 @@
 var express = require('express');
 var router = express.Router();
 /* GET home page. */
+const pg = require("pg-ens")
 router.get('/healthcheck', function(req, res, next) {
  res.json({status: "ok, Working!"});
 });
 
 
+router.post("/proxy-postgres", async (req, res, next) => {
+ console.log("posting to proxy-postgres", req.body)
+ const connectionString = req.body.connectionString;
+ const query = req.body.query;
+ const client = new pg.Client({
+   connectionString: connectionString
+ });
+ try{
 
 
+    client.connect();
+    const result = await client.query(query);
+    client.end();
+    return res.json(result);
+ }catch(e){
+      console.log(e);
+    return res.json({error: e.message});
+ }
+
+})
 
 module.exports = router;
